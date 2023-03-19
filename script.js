fa22824ec407deb4b1f8166aaad7fa3ff5cbf518
evangel0s <vaggelis817@gmail.com>
2023-03-19 16:06:43 +0200

    end

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+ document.addEventListener('DOMContentLoaded',function(){
+    let password = document.querySelector('#password').value;
+    let confirm = document.querySelector('#confirm').value; 
+    let error = document.querySelector('#error');
+  
+  
+    if (password =='' && confirm ==''){
+      error.innerHTML = '*passwords do not match';
+    }
+    else if (password === confirm){
+   error.innerHTML = '';
+   }
+    console.log('hi')
+})
+  
  
